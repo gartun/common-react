1008e03122e1503b79938e168c50c3a1f4ac3563
artgökhan <62377029+gartun@users.noreply.github.com>
2021-10-18 23:23:14 +0300

    Create usePrefersReducedMotion.js

---
+++ usePrefersReducedMotion.js
@@ -0,0 +1,30 @@
+/**
+ * By: Josh Comeau
+ * url: https://www.joshwcomeau.com/snippets/react-hooks/use-prefers-reduced-motion/
+ */
+import { useState, useEffect } from "react";
+
+const QUERY = '(prefers-reduced-motion: no-preference)';
+const isRenderingOnServer = typeof window === 'undefined';
+
+const getInitialState = () => {
+  return isRenderingOnServer ? true : !window.matchMedia(QUERY).matches;
+};
+function usePrefersReducedMotion() {
+  const [prefersReducedMotion, setPrefersReducedMotion] = useState(
+    getInitialState
+  );
+  
+  useEffect(() => {
+    const mediaQueryList = window.matchMedia(QUERY);
+    const listener = (event) => {
+      setPrefersReducedMotion(!event.matches);
+    };
+    mediaQueryList.addEventListener('change', listener);
+    return () => {
+      mediaQueryList.removeEventListener('change', listener);
+    };
+  }, []);
+  
+  return prefersReducedMotion;
+}
